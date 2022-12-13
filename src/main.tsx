ab9f6a5375151f250aced073d55bb04cca7755b4
Shreyansh-Sheth <shethshreyansh777@gmail.com>
2022-12-14 02:06:58 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-
+(window as any).global = window;
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThirdwebProvider desiredChainId={ChainId.Mumbai}>
     <React.StrictMode>
